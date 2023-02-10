099a81991f26a722f9ba855606d40d792e139a44
soyombo <soyombo0@naver.com>
2023-02-10 12:03:12 +0300

    Initial Commit

---
+++ FZZ/FZZApp.swift
@@ -0,0 +1,17 @@
+//
+//  FZZApp.swift
+//  FZZ
+//
+//  Created by Soyombo Mantaagiin on 10.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FZZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
